4ac743f67d38bfc6219de51b0f4fd824af689cf3
Grzesiek <gzaber@tlen.pl>
2024-10-04 22:58:08 +0200

    fix: add notification channel to send notifications

--- app/src/main/java/com/gzaber/remindme/data/service/DefaultNotificationService.kt
+++ app/src/main/java/com/gzaber/remindme/data/service/DefaultNotificationService.kt
@@ -1,16 +1,32 @@
 package com.gzaber.remindme.data.service
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getString
+import com.gzaber.remindme.R
 
 class DefaultNotificationService(
     private val notificationManager: NotificationManager,
     private val context: Context
 ) : NotificationService {
 
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NotificationService.CHANNEL_ID,
+                getString(context, R.string.notification_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
     override fun send(id: Int, title: String, content: String) {
         val notification = NotificationCompat.Builder(context, NotificationService.CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle(title)
             .setContentText(content)
             .build()
