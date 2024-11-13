73a3a0bbddaa7d939d6cf8295495b695b61a4a65
Grzesiek <gzaber@tlen.pl>
2024-11-13 21:21:55 +0100

    refactor: rename loading box to loading indicator, add content description to it

--- app/src/main/java/com/gzaber/remindme/ui/reminders/composable/LoadingIndicator.kt
+++ app/src/main/java/com/gzaber/remindme/ui/reminders/composable/LoadingIndicator.kt
@@ -1,5 +1,6 @@
 package com.gzaber.remindme.ui.reminders.composable
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
@@ -8,17 +9,25 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.gzaber.remindme.R
 
 @Composable
-fun LoadingBox(
+fun LoadingIndicator(
     contentPadding: PaddingValues,
     modifier: Modifier = Modifier,
-    indicatorSize: Dp = 64.dp
+    indicatorSize: Dp = 64.dp,
+    @StringRes indicatorDescription: Int = R.string.loading_indicator_content_description
 ) {
+    val indicatorContentDescription = stringResource(indicatorDescription)
     Box(
-        modifier = modifier.padding(contentPadding),
+        modifier = modifier
+            .padding(contentPadding)
+            .semantics { contentDescription = indicatorContentDescription },
         contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator(
