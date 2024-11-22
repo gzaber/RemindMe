970b33f65c7572c2684289c032c01987210a648f
Grzesiek <gzaber@tlen.pl>
2024-11-22 23:22:45 +0100

    test: create addedit composables tests

---
+++ app/src/test/java/com/gzaber/remindme/ui/addedit/composable/AddEditAppBarTest.kt
@@ -0,0 +1,81 @@
+package com.gzaber.remindme.ui.addedit.composable
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.gzaber.remindme.R
+import com.gzaber.remindme.helper.RobolectricTestActivity
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+@RunWith(RobolectricTestRunner::class)
+class AddEditAppBarTest {
+
+    private val context = RuntimeEnvironment.getApplication()
+    private val titleRes = R.string.add_edit_app_bar_create_title
+    private var isNavigateBackButtonClicked = false
+    private var isSaveButtonClicked = false
+
+    @get:Rule(order = 0)
+    val robolectricTestActivityRule = RobolectricTestActivity()
+
+    @get:Rule(order = 1)
+    val composeTestRule = createComposeRule()
+
+    private fun setUpAppBar(isLoading: Boolean = false) {
+        composeTestRule.setContent {
+            AddEditAppBar(
+                title = titleRes,
+                isLoading = isLoading,
+                onNavigateBack = { isNavigateBackButtonClicked = true },
+                onSave = { isSaveButtonClicked = true }
+            )
+        }
+    }
+
+    @Test
+    fun title_isDisplayed() {
+        setUpAppBar()
+
+        composeTestRule.onNodeWithText(context.getString(titleRes)).assertIsDisplayed()
+    }
+
+    @Test
+    fun isLoading_circularProgressIndicatorIsDisplayed() {
+        setUpAppBar(isLoading = true)
+
+        composeTestRule.onNodeWithContentDescription(
+            context.getString(R.string.loading_indicator_content_description)
+        ).assertIsDisplayed()
+    }
+
+    @Test
+    fun backButton_clicked_callsOnNavigateBack() {
+        setUpAppBar()
+
+        composeTestRule
+            .onNodeWithContentDescription(context.getString(R.string.add_edit_close_icon_description))
+            .assertIsDisplayed()
+            .performClick()
+
+        assertTrue(isNavigateBackButtonClicked)
+    }
+
+    @Test
+    fun saveButton_clicked_callsOnSave() {
+        setUpAppBar()
+
+        composeTestRule
+            .onNodeWithContentDescription(context.getString(R.string.add_edit_save_icon_description))
+            .assertIsDisplayed()
+            .performClick()
+
+        assertTrue(isSaveButtonClicked)
+    }
+}
---
+++ app/src/test/java/com/gzaber/remindme/ui/addedit/composable/AdvancePickerModalTest.kt
@@ -0,0 +1,96 @@
+package com.gzaber.remindme.ui.addedit.composable
+
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsSelectable
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.gzaber.remindme.R
+import com.gzaber.remindme.helper.RobolectricTestActivity
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+@RunWith(RobolectricTestRunner::class)
+class AdvancePickerModalTest {
+
+    private val context = RuntimeEnvironment.getApplication()
+    private val advanceUnits = listOf("minute", "hour", "day")
+    private var isCloseButtonClicked = false
+    private var advanceUnit = 0
+    private var advanceValue = 0
+
+    @get:Rule(order = 0)
+    val robolectricTestActivityRule = RobolectricTestActivity()
+
+    @get:Rule(order = 1)
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            AdvancePickerModal(
+                advanceUnits = advanceUnits,
+                selectedAdvanceUnit = "minute",
+                advanceValues = (1..5).toList(),
+                selectedAdvanceValue = 0,
+                onAdvanceUnitSelected = { advanceUnit = it },
+                onAdvanceValueSelected = { advanceValue = it },
+                onDismiss = { isCloseButtonClicked = true },
+            )
+        }
+    }
+
+    @Test
+    fun title_isDisplayed() {
+        composeTestRule.onNodeWithText(context.getString(R.string.add_edit_advance_title))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun closeButton_clicked_callsOnDismiss() {
+        composeTestRule.onNodeWithText(context.getString(R.string.close_button_text))
+            .assertIsDisplayed()
+            .performClick()
+
+        assertTrue(isCloseButtonClicked)
+    }
+
+    @Test
+    fun listOfAdvanceUnits_isDisplayedAndItemsAreSelectable() {
+        with(composeTestRule) {
+            onNodeWithText("minute").assertIsDisplayed().assertIsSelectable()
+            onNodeWithText("hour").assertIsDisplayed().assertIsSelectable()
+            onNodeWithText("day").assertIsDisplayed().assertIsSelectable()
+        }
+    }
+
+    @Test
+    fun listOfAdvanceValues_isDisplayedAndItemsAreClickable() {
+        with(composeTestRule) {
+            for (i in 1..5) {
+                onNodeWithText("$i").assertIsDisplayed().assertHasClickAction()
+            }
+        }
+    }
+
+    @Test
+    fun advanceUnit_clicked_callsOnAdvanceUnitSelected() {
+        composeTestRule.onNodeWithText("day").performClick()
+
+        assertEquals(2, advanceUnit)
+    }
+
+    @Test
+    fun advanceValue_clicked_callsOnAdvanceValueSelected() {
+        composeTestRule.onNodeWithText("5").performClick()
+
+        assertEquals(5, advanceValue)
+    }
+}
